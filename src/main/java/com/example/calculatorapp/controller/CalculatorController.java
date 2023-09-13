5033c350ff3a35798a2cf5d1497fede976080aba
conditional <105pkn@mail.ru>
2023-09-13 13:19:32 +0700

    init commit

--- src/main/java/com/example/calculatorapp/controller/CalculatorController.java
+++ src/main/java/com/example/calculatorapp/controller/CalculatorController.java
@@ -39,9 +39,9 @@ public class CalculatorController {
 
     @GetMapping("/divide")
     public String divide(@RequestParam int num1, @RequestParam int num2) {
-        if (num2 == 0) {
-            return "Нельзя делить на 0";
-        }
+//        if (num2 == 0) {
+//            return "Нельзя делить на 0";
+//        }
         return num1 + " / " + num2 + " = " + calculatorService.divide(num1, num2);
     }
 }
--- src/main/java/com/example/calculatorapp/service/CalculatorServiceImpl.java
+++ src/main/java/com/example/calculatorapp/service/CalculatorServiceImpl.java
@@ -29,6 +29,9 @@ public class CalculatorServiceImpl implements CalculatorService {
 
     @Override
     public double divide(int num1, int num2) {
+                if (num2 == 0) {
+            throw new IllegalArgumentException();
+        }
         double result = (double) num1 / num2;
         return result;
     }
